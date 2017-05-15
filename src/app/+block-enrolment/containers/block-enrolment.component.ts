23cabf44295a2ad2de672db6cd9db04f6c73f1c0
JoeC <placid.joe@gmail.com>
2017-05-15 19:49:09 +1200

    Show state on container

--- src/app/+block-enrolment/containers/block-enrolment.component.ts
+++ src/app/+block-enrolment/containers/block-enrolment.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { getBlocksForEnrolmentState } from '../../services/redux/blocks-for-enrolment/blocks-for-enrolment.selectors';
+import { AppState } from '../../services/redux/app/app.model';
+import { Store } from '@ngrx/store';
+import { BlocksForEnrolmentState } from '../../services/redux/blocks-for-enrolment/blocks-for-enrolment.model';
+import { Observable } from 'rxjs/Rx';
+import { Component, OnInit } from '@angular/core';
 
 /**
  * Block enrolment for users
@@ -7,8 +12,15 @@ import { Component } from '@angular/core';
     selector: 'fs-block-enrolment',
     template: `
         <h2>Block Enrolment</h2>
+        <pre>{{ state$ | async | json }}</pre>
     `
 })
-export class BlockEnrolmentComponent {
+export class BlockEnrolmentComponent implements OnInit {
+    public state$: Observable<BlocksForEnrolmentState>;
 
+    constructor(private _store: Store<AppState>) {}
+
+    public ngOnInit() {
+        this.state$ = this._store.select(getBlocksForEnrolmentState);
+    }
 }
