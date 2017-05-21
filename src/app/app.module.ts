72a584af512df2e797c123aea035eb5e4a973692
JoeC <placid.joe@gmail.com>
2017-05-21 17:14:03 +1200

    Shifted redux to own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,11 @@
-import { ApiModule } from './services/apis/api.module';
-import { BlocksForEnrolmentsEffects } from './services/redux/blocks-for-enrolment/blocks-for-enrolment.effects';
-import { CurrentPassesEffects } from './services/redux/current-passes/current-passes.effects';
-import { UpcomingScheduleEffects } from './services/redux/upcoming-schedule/upcoming-schedule.effects';
-import { UserEffects } from './services/redux/user/user.effects';
-import { userReducer } from './services/redux/user/user.reducer';
-import { appReducer } from './services/redux/app/app.reducer';
-import { CommonModule } from './common/common.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http } from '@angular/http';
-import {
-  NgModule,
-  ApplicationRef
-} from '@angular/core';
-import {
-  removeNgStyles,
-  createNewHosts,
-  createInputTransfer
-} from '@angularclass/hmr';
-import {
-  RouterModule,
-  PreloadAllModules,
-  NoPreloading
-} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, NoPreloading } from '@angular/router';
+import { ReduxModule } from './services/redux/redux.module';
+import { ApiModule } from './services/apis/api.module';
+import { CommonModule } from './common/common.module';
 
 /*
  * Platform and Environment providers/directives/pipes
@@ -36,11 +19,6 @@ import { NoContentComponent } from './no-content';
 
 import '../styles/styles.scss';
 import '../styles/headings.css';
-
-import { StoreModule } from '@ngrx/store';
-import { RouterStoreModule } from '@ngrx/router-store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
 
 // Application wide providers
 const APP_PROVIDERS = [
@@ -62,15 +40,9 @@ const APP_PROVIDERS = [
     HttpModule,
     RouterModule.forRoot(ROUTES, { useHash: false, preloadingStrategy: NoPreloading }),
     ApiModule,
+    ReduxModule,
     CommonModule,
 
-    StoreModule.provideStore(appReducer),
-    RouterStoreModule.connectRouter(),
-    StoreDevtoolsModule.instrumentOnlyWithExtension(),
-    EffectsModule.run(UserEffects),
-    EffectsModule.run(UpcomingScheduleEffects),
-    EffectsModule.run(CurrentPassesEffects),
-    EffectsModule.run(BlocksForEnrolmentsEffects),
   ],
   providers: [ // expose our Services and Providers into Angular's dependency injection
     ENV_PROVIDERS,
